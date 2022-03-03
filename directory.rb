9d79cb4b7c5f46e95401cb5817d621fff0341129
Bradley Deakin <bradleydeakin2@gmail.com>
2022-03-03 15:27:03 +0000

    methods

--- directory.rb
+++ directory.rb
@@ -13,9 +13,18 @@ students = [
 "Joffrey Baratheon",
 "Norman Bates"
 ]
-puts"The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-    puts student
+def print_header
+    puts"The students of Villains Academy"
+    puts "-------------"
 end
-print "overall we have #{students.count} great students"
+def print(names)
+    names.each do |name|
+        puts name
+    end
+end
+def print_footer(names)
+    puts "overall we have #{names.count} great students"
+end
+print_header
+print(students)
+print_footer(students)
