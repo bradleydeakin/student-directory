fe3d4783e4094c4521738e4f9f83002aca0e6e51
Bradley Deakin <bradleydeakin2@gmail.com>
2022-03-03 15:19:40 +0000

    refactored student

--- directory.rb
+++ directory.rb
@@ -1,15 +1,21 @@
-student_number = 11
-puts "The students of Villains Academy"
+
+students = [
+
+"Dr. Hannibal Lecter",
+"Darth Vader",
+"Nurse Ratched",
+"Michael Corleone",
+"Alex DeLarge",
+"The Wicked Witch of the West",
+"Terminator",
+"Freddy Krueger",
+"The Joker",
+"Joffrey Baratheon",
+"Norman Bates"
+]
+puts"The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-print "overall we have #{student_number} great students"
+students.each do |student|
+    puts student
+end
+print "overall we have #{students.count} great students"
