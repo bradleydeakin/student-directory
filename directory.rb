91eef3280781c39fc892e22d820a5e2ec217e6eb
Bradley Deakin <bradleydeakin2@gmail.com>
2022-03-04 16:39:09 +0000

    refactored menu and saving files

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-def input_students(students)
+def input_students
   puts "would you like to enter students"
   answer = gets.chomp
   no_students = false
@@ -9,7 +9,7 @@ def input_students(students)
   end
   puts "Please enter the names of the students"
   
-  students = students
+  
 
   name = gets.chomp
   if name == " "
@@ -26,11 +26,11 @@ def input_students(students)
     height += "unknown"
   end
   while true do
-    students << {name: name, cohort: cohort, height: height}
-    if students.length == 1
+    @students << {name: name, cohort: cohort, height: height}
+    if @students.length == 1
         puts "now we have 1 student"
     else
-        puts "now we have #{students.count} students"
+        puts "now we have #{@students.count} students"
     end
     puts "would you like to enter more students"
     decision = gets.chomp
@@ -55,7 +55,7 @@ def input_students(students)
     end
   end
 
-  return students
+  
 end
 # students = [
 
@@ -71,27 +71,27 @@ end
 #     {name: "Joffrey Baratheon", cohort: :november},
 #     {name: "Norman Bates", cohort: :november}
 # ]
-def print_header(students)
-    if students.length > 0
+def print_header
+    if @students.length > 0
         puts"The students of Villains Academy"
         puts "-------------"
     else
         puts "no data to display"
     end
 end
-def print(students)
-    if students.length > 0
-        students.each_with_index do |student, index|
+def print_student_list
+    if @students.length > 0
+        @students.each_with_index do |student, index|
             puts "#{index + 1}. #{student[:name]} (cohort = #{student[:cohort]}, height = #{student[:height]})"
         end
     end
 end
 
-def while_print(students)
-    if students.length > 0
+def while_print
+    if @students.length > 0
         count = 0
-        while count != students.length do
-            students.each do |student|
+        while count != @students.length do
+            @students.each do |student|
                 puts "#{student[:name]}".center(20)
                 count += 1
             end
@@ -99,54 +99,73 @@ def while_print(students)
     end
 end
 
-def feb_cohort(students)
-    if students.length > 0
-        feb = students.select{ |student| student[:cohort] == "feb" }
+def feb_cohort
+    if @students.length > 0
+        feb = @students.select{ |student| student[:cohort] == "feb" }
         puts feb 
     end
 end
 
-def print_begins_D(students)
-    if students == false
-        d = students.select {|name| name[:name].start_with?('d') }
+def print_begins_D
+    if @students == false
+        d = @students.select {|name| name[:name].start_with?('d') }
         puts d
     end
 end
-def less_than_12(students)
-    if students == false
-        twelve = students.select { |name| name[:name].length < 12 }
+def less_than_12
+    if @students == false
+        twelve = @students.select { |name| name[:name].length < 12 }
         puts twelve
     end
 end
 
-def print_footer(students)
-    if students.length > 0
-      puts "overall we have #{students.count} great students"
+def print_footer
+    if @students.length > 0
+      puts "overall we have #{@students.count} great students"
     end
 end
-
-def interactive_menu
-    students = []
-    while true do
-        puts "Select: \n 1. input students\n 2. show students\n 3. 9. Exit"
-        selcetion = gets.chomp
-        case selcetion
+def show_students
+    print_header
+    print_student_list
+    # feb_cohort
+    # while_print
+    print_footer
+    # less_than_12
+end
+def process(selection)
+    case selection
         when "1"
-            input_students(students)
+            input_students
         when "2"
-            print_header(students)
-            print(students)
-            # feb_cohort(students)
-            # while_print(students)
-            print_footer(students)
-            # less_than_12(students)
+            show_students
+        when "3"
+            save_students
         when "9"
             exit
         else
             "Sorry didnt understand"
-        end
+    end
+end        
+def print_menu
+    puts "Select: \n 1. input students\n 2. show students\n 3. save students to file\n 9. Exit"
+end
+def interactive_menu
+    @students = []
+    while true do
+        print_menu
+        # selection = get.chomp
+        process(gets.chomp)        
     end
 end
 
+def save_students
+    file = File.open("students.csv", "w")
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort], student[:height]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+    file.close
+end
 interactive_menu
 
